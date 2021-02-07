d6a8f74b00ab5cb76f15176a3e30efb297cc83b1
saleh898 <theone@theone-PC>
2021-02-07 06:33:40 +0300

    f

--- MyName/src/MyName.java
+++ MyName/src/MyName.java
@@ -4,6 +4,8 @@ public class MyName {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
    System.out.println("My Name is Saleh");
+   System.out.println("My Age is 24 Years");
+
 	}
 
 }
